542c424a4871983eca3d17d3d99857def1a03765
selfisch <selfisch@gmx.de>
2017-08-16 20:45:04 +0200

    ein paar kommentierte Zeilen entfernt

--- main.py
+++ main.py
@@ -14,26 +14,23 @@ client.timeout = 10     # network timeout in seconds (floats allowed), default: 
 client.idletimeout = None
 client.connect("localhost", 6600)
 
+# zweite Maus für Test an meinem Rechner
 #dev = InputDevice('/dev/input/event13') # This can be any other event number. On$
+
+# Maus an der Aiwa
 dev = InputDevice('/dev/input/event0') # This can be any other event number. On$
 
 while True:
     r,w,x = select([dev], [], [])
     for event in dev.read():
-    # The event.code for a scroll wheel event is 8, so I do the following
-    # button left 272, right 273
         if event.code == 8:
             #print "wheel"
-            #print(event.value)
             if event.value == 1:
                 os.system("amixer -q sset Master 1%+")
             elif event.value == -1:
                 os.system("amixer -q sset Master 1%-")
         elif event.code == 272 and event.value == 1:
             #print "linker Knopf"
-            #print(event.value)
-            #print(event.code)
-            #client.next()
             state = client.status()['state'].split(":")
             if 'play' in state:
                 client.pause()
