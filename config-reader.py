dc387e5a281cc19e6b27b5b416fb4f58ee1b6b3a
selfisch <selfisch@gmx.de>
2017-09-16 12:39:22 +0200

    Konfiguration für den Reader

---
+++ config-reader.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import os.path
+from evdev import InputDevice, list_devices
+
+devices = [InputDevice(fn) for fn in list_devices()]
+path = os.path.dirname(os.path.realpath(__file__))
+i = 0
+print("Choose the reader from list")
+for dev in devices:
+	print(i, dev.name)
+	i += 1
+
+dev_id = int(input('Device Number: '))
+
+with open(path + '/conf/reader.py','w') as f:
+	f.write(devices[dev_id].name)
+	f.close()
