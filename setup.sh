6d1e989acf689a1692496d7683418d8734c76bf3
selfisch <selfisch@gmx.de>
2017-10-13 12:55:53 +0200

    .

--- setup.sh
+++ setup.sh
@@ -2,42 +2,39 @@
 
 hostname=`cat /etc/hostname`
 
-cd ~
+cur_dir=`pwd`
 
-mkdir -p ~/domrpi-ki
+# check sudo
+ sudo su
 
 # Mopidy sources
  wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
  sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/stretch.list
 
 # apt
- sudo apt-get update
- sudo apt-get purge -y python python-dev python-pip
+ apt-get update
+ apt-get purge -y python python-dev python-pip
 
  sleep 5
 
- sudo apt-get clean
- sudo apt-get autoclean
- sudo apt-get -y autoremove
+ apt-get clean
+ apt-get autoclean
+ apt-get -y autoremove
 
  sleep 5
 
- sudo apt-get install -y mopidy libspotify12 libspotify-dev \
+ apt-get install -y mopidy libspotify12 libspotify-dev \
  git python3 python3-dev python3-pip libffi-dev python3-spotify
 
  sleep 5
 
 # pip
- sudo pip3 install evdev python-mpd2 Mopidy-Iris pyspotify mopidy-spotify
-
-# git
- cd ~/domrpi-ki
- git clone https://github.com/selfisch/domrpi-ki.git
+ pip3 install evdev python-mpd2 Mopidy-Iris pyspotify mopidy-spotify
 
 # mopidy config
-mv /etc/mopidy/mopidy.conf /etc/mopidy/mopidy.bak
-touch /etc/mopidy/mopidy.conf
-chown mopidy:root /etc/mopidy/mopidy.conf
+ mv /etc/mopidy/mopidy.conf /etc/mopidy/mopidy.bak
+ touch /etc/mopidy/mopidy.conf
+ chown mopidy:root /etc/mopidy/mopidy.conf
 
 echo "[core]
 cache_dir = /var/cache/mopidy
@@ -67,10 +64,16 @@ media_dir = /var/lib/mopidy/media
 [m3u]
 playlists_dir = /var/lib/mopidy/playlists" > /etc/mopidy/mopidy.conf
 
-if [ -f ~/domrpi-ki/conf/spotify.conf ] then
- exec  ~/domrpi-ki/conf/spotify.conf
+if [ -f $cur_dir/conf/spotify.conf ] then
+ exec $cur_dir/conf/spotify.conf
 else
+ echo ""
  echo "Wenn Spotify in Mopidy genutzt werden soll, bitte die Datei
- ~/domrpi-ki/conf/spotify.template beachten."
+ $cur_dir/conf/spotify.template beachten."
+
+if [ ! -f $cur_dir/plist.csv ] then
+ echo ""
+ echo "Bitte plist.template beachten!"
+fi
 
 systemctl enable mopidy
