466bf341c020d6506fdafab054d8c3dcee378938
selfisch <selfisch@gmx.de>
2017-10-13 12:57:17 +0200

    .

--- setup.sh
+++ setup.sh
@@ -7,33 +7,33 @@ cur_dir=`pwd`
 # check sudo
  sudo su
 
-# Mopidy sources
- wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
- sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/stretch.list
-
-# apt
- apt-get update
- apt-get purge -y python python-dev python-pip
-
- sleep 5
- 
- apt-get -y dist-upgrade
-
- sleep 5
-
- apt-get clean
- apt-get autoclean
- apt-get -y autoremove
-
- sleep 5
-
- apt-get install -y mopidy libspotify12 libspotify-dev \
- git python3 python3-dev python3-pip libffi-dev python3-spotify
-
- sleep 5
-
-# pip
- pip3 install evdev python-mpd2 Mopidy-Iris pyspotify mopidy-spotify
+# # Mopidy sources
+#  wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+#  sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/stretch.list
+#
+# # apt
+#  apt-get update
+#  apt-get purge -y python python-dev python-pip
+#
+#  sleep 5
+#
+#  apt-get -y dist-upgrade
+#
+#  sleep 5
+#
+#  apt-get clean
+#  apt-get autoclean
+#  apt-get -y autoremove
+#
+#  sleep 5
+#
+#  apt-get install -y mopidy libspotify12 libspotify-dev \
+#  git python3 python3-dev python3-pip libffi-dev python3-spotify
+#
+#  sleep 5
+#
+# # pip
+#  pip3 install evdev python-mpd2 Mopidy-Iris pyspotify mopidy-spotify
 
 # mopidy config
  mv /etc/mopidy/mopidy.conf /etc/mopidy/mopidy.bak
