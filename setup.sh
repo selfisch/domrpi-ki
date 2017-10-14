c49ef1190dac2acc08a58c26f38e62a985bc5995
selfisch <selfisch@gmx.de>
2017-10-14 11:42:03 +0200

    .

--- setup.sh
+++ setup.sh
@@ -11,7 +11,6 @@ cur_dir=`pwd`
  wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
  sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/stretch.list
 
-
 # apt
  sudo apt-get update
  sudo apt-get purge -y python python-dev python-pip
