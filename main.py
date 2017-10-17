0a53c0c35cc39a77c5711e128d4762dc5065fb4b
selfisch <selfisch@gmx.de>
2017-10-17 08:14:04 +0200

    .

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ try:
         _thread.start_new_thread(cardreader.read_card('read_card', True, ) )
     if not check_mouse == 'n':
         _thread.start_new_thread(mouse.mouse_press('mouse_press', True, ) )
-    if not check_usbbtn = 'n':
+    if not check_usbbtn == 'n':
         _thread.start_new_thread(usbbtn.button_press('button_press', True, ) )
 except (SystemExit):
     logger.info("Anwendung beendet")
