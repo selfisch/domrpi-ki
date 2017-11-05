7223b10628464e8d854d699ed8a433160c0be727
selfisch <selfisch@gmx.de>
2017-11-05 22:51:54 +0100

    added function to powerbutton py

--- conf/func_powerbtn.py
+++ conf/func_powerbtn.py
@@ -12,14 +12,15 @@ GPIO.setup(5, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 oldButtonState1 = True
 
-while True:
-    #grab the current button state
-    buttonState1 = GPIO.input(5)
+def PowerBTN:
+    while True:
+        #grab the current button state
+        buttonState1 = GPIO.input(5)
 
-  # check to see if button has been pushed
-  if buttonState1 != oldButtonState1 and buttonState1 == False:
-    subprocess.call("shutdown -h now", shell=True,
-      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    oldButtonState1 = buttonState1
+        # check to see if button has been pushed
+        if buttonState1 != oldButtonState1 and buttonState1 == False:
+            subprocess.call("shutdown -h now", shell=True,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            oldButtonState1 = buttonState1
 
-    time.sleep(1)
+        time.sleep(1)
